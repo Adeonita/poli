75f3a7f41d88b500cd2f62a832bbd8fa2bd1fac6
adeonita <adeonita.sousa@gmail.com>
2020-07-15 23:26:28 -0300

    Revert "input dinamico para circle, triangle, squad e rectangle"
    This reverts commit 50206c01ad064142ea55dce771163bc8e3a22212.

--- src/input.ts
+++ src/input.ts
@@ -4,9 +4,9 @@
 //      console.log('input', this.value);
 //     });
 
-let inputNumberEdges = document.getElementById('input-number-of-edges') as HTMLInputElement; //input od edge
-let btnNumberEdges = document.getElementById('btn-number-of-edges') as HTMLInputElement;  //submit
+let submit = document.querySelector('button') as HTMLInputElement; 
 let select = document.getElementById('select-polygon') as HTMLSelectElement ;
+let input = document.getElementById('arestas') as HTMLInputElement; //input od edge
 let canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
 let figure: {name: string, value: any}[] = [
@@ -14,7 +14,7 @@ let figure: {name: string, value: any}[] = [
     {name: 'Triangle', value: 3},
     {name: 'Squad', value: 4},
     {name: 'Rectangle', value: 42},
-    {name: 'Others figures', value: 0},
+    {name: 'Others', value: 0},
 ];
 
 for (const option of figure) {
@@ -44,108 +44,52 @@ let amoutEdge;
 select.onclick = () => { //vindo do select
 
     let edge = select.options[select.selectedIndex].value; //get courrent value selected
-    let singleInputDiv = document.getElementById('single-input-div');
-    let descriptionCircle = document.getElementById('description-circle');
-    let descriptionOthers = document.getElementById('description-others');
-    let singleInput = document.getElementById('single-input') as HTMLInputElement; //para poligonos regulares
-    let btnSingleInput = document.getElementById('btn-single-input');  
-    let doubleInputDiv = document.getElementById('double-input-div'); 
-    let doubleInputBase = document.getElementById('double-input-base') as HTMLInputElement; //para retângulos
-    let doubleInputHeight = document.getElementById('double-input-height') as HTMLInputElement; //para retângulos
-    let btnDoubleInput = document.getElementById('btn-double-input');
+    let placeholderRegularMeasures = document.getElementById('placeholder-regular-measures'); //placeholder para input do tamanho das arestas
     let inputRegularMeasures = document.getElementById('input-regular-measures') as HTMLInputElement; //o proprio input
     let btnRegularMeasures = document.getElementById('btn-regular-measures');
-    let labelBase = document.getElementById('label-base');
-    let labelHeight = document.getElementById('label-height');
-    let descriptionDoubleInput =  document.getElementById('description-double-input');
-    
-    if(edge == '42'){ //Se for retangulo
-        if(singleInputDiv?.classList.contains('d-block')){
-            singleInputDiv?.classList.remove('d-block');
-            singleInputDiv?.classList.add('d-none');
+
+    if(edge == '0'){
+        submit?.classList.remove('d-none');
+        input?.classList.remove('d-none');
+        input?.classList.add('d-inline-block');
+    } else {
+        if( !input?.classList.contains('d-none') ) {
+            input?.classList.remove('d-inline-block');
+            input?.classList.add('d-none');
+            submit?.classList.add('d-none');
         }
-        doubleInputDiv?.classList.add('d-block');
-        btnDoubleInput.onclick = () => { 
-            let dib = doubleInputBase.value;
-            let dih  = doubleInputHeight.value;
-            console.log(`Base: ${dib} Altura: ${dih}`);
+    }if(edge == '4'){
+        inputRegularMeasures?.classList.remove('d-none');
+        inputRegularMeasures?.classList.add('d-inline-block');
+        btnRegularMeasures?.classList.remove('d-none');
+        btnRegularMeasures.onclick = () => {
+            console.log(inputRegularMeasures?.value)
         }
-    }else{ //Se for qualquer outra figura
-        if(doubleInputDiv?.classList.contains('d-block')){
-            doubleInputDiv.classList.remove('d-block');
-            doubleInputDiv.classList.add('d-none');
-        }
-        if(edge != '0'){ 
-            if(edge == '1'){
-                descriptionCircle?.classList.remove('d-none');
-                descriptionOthers?.classList.add('d-none');
-            }else{
-                descriptionCircle?.classList.add('d-none');
-                descriptionOthers?.classList.remove('d-none');
-            }
-            singleInputDiv?.classList.add('d-block');
+    }else{
+        if( !inputRegularMeasures?.classList.contains('d-none') ){
+            inputRegularMeasures?.classList.remove('d-inline-block');
+            inputRegularMeasures?.classList.add('d-none');
+            btnRegularMeasures?.classList.add('d-none');
         }
     }
-    // if(edge == '0'){
-    //     btnNumberEdges?.classList.remove('d-none');
-    //     inputNumberEdges?.classList.remove('d-none');
-    //     inputNumberEdges?.classList.add('d-inline-block');
-    // } else {
-    //     if( !inputNumberEdges?.classList.contains('d-none') ) {
-    //         inputNumberEdges?.classList.remove('d-inline-block');
-    //         inputNumberEdges?.classList.add('d-none');
-    //         btnNumberEdges?.classList.add('d-none');
-    //     }
-    // }if(edge == '42'){
-    //     // inputRegularMeasures?.classList.remove('d-none');
-    //     // inputRegularMeasures?.classList.add('d-inline-block');
-    //     // btnRegularMeasures?.classList.remove('d-none');
-    //     // btnRegularMeasures.onclick = () => {
-    //     //     console.log(inputRegularMeasures?.value)
-    //     // }
-    //     inputRegularMeasures?.classList.remove('d-inline-block');
-    //     inputRegularMeasures?.classList.add('d-none');
-    //     btnRegularMeasures?.classList.add('d-none');
-
-    //     doubleInputBase.classList.remove('d-none');
-    //     doubleInputBase.classList.add('d-inline');
-    //     doubleInputHeight.classList.remove('d-none');
-    //     doubleInputHeight.classList.add('d-inline');
-    //     //remove o anterior
-
-    // }else{
-    //     inputRegularMeasures?.classList.remove('d-none');
-    //     inputRegularMeasures?.classList.add('d-inline-block');
-    //     btnRegularMeasures?.classList.remove('d-none');
-    //     btnRegularMeasures.onclick = () => {
-    //         console.log(inputRegularMeasures?.value)
-    //     }
-    //     doubleInputBase.classList.add('d-none');
-
-    //     // if( !inputRegularMeasures?.classList.contains('d-none') ){
-    //     //     inputRegularMeasures?.classList.remove('d-inline-block');
-    //     //     inputRegularMeasures?.classList.add('d-none');
-    //     //     btnRegularMeasures?.classList.add('d-none');
-    //     // }
-    // }
     
     amoutEdge = edge //do input vem uma string
     console.log(amoutEdge)
 }
 
-inputNumberEdges.addEventListener('input', function() { //valida campo para numero 
+input.addEventListener('input', function() { //valida campo para numero 
     let filter = /^[0-9]+$/;
     let edge = this.value;
     if(!filter.test(edge)){
-        inputNumberEdges.value = edge.substring(0,0);
+       input.value = edge.substring(0,0);
     }if(edge.length > 2){
-        inputNumberEdges.value = edge.substring(2,0);
+        input.value = edge.substring(2,0);
     }
-    amoutEdge = parseInt(inputNumberEdges.value);
+    amoutEdge = parseInt(input.value);
 })
 
-btnNumberEdges.onclick = () => { //vindo do input
-    amoutEdge = parseInt(inputNumberEdges?.value);
+submit.onclick = () => { //vindo do input
+    amoutEdge = parseInt(input?.value);
     console.log(amoutEdge);
 }
 
